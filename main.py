d6af905eba30ea562f8bd6e9eacf0ec57f80a8f7
mari0724 <ximenadelgadom07@gmail.com>
2025-10-20 16:56:45 -0500

    librerias minimas y estructura hexagonal

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
-# Hola aaaaaaaa
-print("Hola desde mi proyecto Python") 
+from fastapi import FastAPI
 
+
+app = FastAPI(title="SENASOFT", version="1.0")
+app.include_router(router)
+
+@app.get("/")
+def home():
+    return {"message": "Bienvenidos a SenaSoft 2025 🚀"}
