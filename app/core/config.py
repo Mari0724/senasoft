ed2d146c63f5408193d489a91c4f0ef98b5814f1
mari0724 <ximenadelgadom07@gmail.com>
2025-10-20 17:14:52 -0500

    Clean .env and update .gitignore

---
+++ app/core/config.py
@@ -0,0 +1,20 @@
+import os
+from dotenv import load_dotenv
+
+# Cargar variables del .env
+load_dotenv()
+
+class Settings:
+    APP_NAME: str = os.getenv("APP_NAME", "SENASOFT")
+    APP_ENV: str = os.getenv("APP_ENV", "development")
+    APP_VERSION: str = os.getenv("APP_VERSION", "1.0.0")
+
+    HOST: str = os.getenv("HOST", "127.0.0.1")
+    PORT: int = int(os.getenv("PORT", 8000))
+    DEBUG: bool = os.getenv("DEBUG", "True") == "True"
+
+    SECRET_KEY: str = os.getenv("SECRET_KEY", "changeme")
+
+    DATABASE_URL: str = os.getenv("DATABASE_URL", None)
+
+settings = Settings()
