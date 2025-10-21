b1148893aef9d2674e4325e31746f969334442e2
Emilia851 <emiliagalloalzate85@gmail.com>
2025-10-21 18:34:10 -0500

    modulo social

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from fastapi import FastAPI
-
+from app.api import social_routes
 
 app = FastAPI(title="SENASOFT", version="1.0")
-
+app.include_router(social_routes.router)
 
 @app.get("/")
 def home():
