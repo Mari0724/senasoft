d856e57ae112bfa8e7d789403af975ffba4be4a2
Emilia851 <emiliagalloalzate85@gmail.com>
2025-10-23 11:32:47 -0500

    video

--- app/application/helpers.py
+++ app/application/helpers.py
@@ -1,6 +1,8 @@
 import os
 import json
 import numpy as np
+import matplotlib
+matplotlib.use("Agg")
 import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
 from sklearn.metrics import (
